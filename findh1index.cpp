1883f13d7beebeb0295d0b6246cee5d81e1c6901
mahak540 <162689029+mahak540@users.noreply.github.com>
2025-06-12 14:58:50 +0530

    Find H1 index

---
+++ findh1index.cpp
@@ -0,0 +1,26 @@
+// User function Template for C++
+class Solution {
+  public:
+    // Function to find hIndex
+    int hIndex(vector<int>& citations) {
+        // code here
+        int n=citations.size();
+        
+        vector<int>freq(n+1);
+        for(int i=0;i<n;i++)
+        {
+            if(citations[i]>=n)
+            freq[n]+=1;
+            else
+            freq[citations[i]]+=1;
+        }
+        int idx=n;
+        int s=freq[n];
+        while(s<idx)
+        {
+            idx--;
+            s+=freq[idx];
+        }
+        return idx;
+    }
+};
